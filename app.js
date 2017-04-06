e7e014a3b68441edb6df585a3af78ce7eea43ad4
AgataKondzior <ak.kondzior@gmail.com>
2017-04-06 22:22:22 +0200

    add

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 var main = function () 
 { 
     
-    $(".active").click(function()
-{     
-    
+    $('.article').click(function() {     
+     $('.article').removeClass('current');
+     $('.hidden').hide();
         
-    $(".description").hide();  
-    $(".current").show();
-    
-    var currentArticle = $(".current");  
-    var nextArticle = currentArticle.next();  
-        
-    currentArticle.removeClass("current");
-    nextArticle.addClass("current");
-        
-    
+    $(this).addClass('current');
+    $(this).children('.hidden').toggle("slow");   
            
 });
  
+  $(document).keypress(function(event) {
+    if(event.which === 111) {
+      $('.hidden').hide();
+
+      $('.current').children('.hidden').toggle("slow");
+    }
+
+    else if(event.which === 110) {
+      var currentArticle = $('.current');
+      var nextArticle = currentArticle.next();
+      
+      currentArticle.removeClass('current');
+      nextArticle.addClass('current');
+    }
+    
+  });
 };
 
 $(document).ready(main);
