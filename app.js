5ccc5693310a4304c5c77a5ea6a0d6789973230a
AgataKondzior <ak.kondzior@gmail.com>
2017-04-10 09:59:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,27 +2,29 @@ var main = function ()
 { 
     
     $('.article').click(function() {     
-     $('.article').removeClass('current');
-     $('.hidden').hide();
+         $('.article').removeClass('current');
+         $('.hidden').hide();
         
-    $(this).addClass('current');
-    $(this).children('.hidden').show();   
+         $(this).addClass('current');
+         $(this).children('.hidden').show();   
            
 });
  
-  $(document).keypress(function(event) {
-    if(event.which === 111) {
-      $('.hidden').hide();
-
-      $('.current').children('.hidden').show();
+    $(document).keypress(function(event) {
+    
+        if(event.which === 111) {
+            
+        $('.hidden').hide();
+        $('.current').children('.hidden').show();
     }
 
-    else if(event.which === 110) {
-      var currentArticle = $('.current');
-      var nextArticle = currentArticle.next();
+        else if(event.which === 110) {
+            
+        var currentArticle = $('.current');
+        var nextArticle = currentArticle.next();
       
-      currentArticle.removeClass('current');
-      nextArticle.addClass('current');
+        currentArticle.removeClass('current');
+        nextArticle.addClass('current');
     }
     
   });
